3e3eba7742427d166368eae2b1c77cf002d6d6cb
root <root@master1>
2022-08-23 22:23:57 +0800

    nginx test

--- site-modules/profile/manifests/seat_movement.pp
+++ site-modules/profile/manifests/seat_movement.pp
@@ -3,3 +3,19 @@ class profile::seat_movement(
     String $pg_password = '123',
 ) {
     class { 'nginx': }
+    group { 'seatmovement':
+        ensure => present,
+    }
+    user { 'seatmovement':
+        ensure     => present,
+        forcelocal => true,
+        system     => true,
+        managehome => true,
+        gid        => 'seatmovement',
+    }
+    file { '/var/log/seatmovement':
+        ensure => directory,
+        mode   => '0644',
+        owner  => 'nginx',
+        group  => 'root',
+    }
